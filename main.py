3bf2cfc395e9af92a1b6780d753b53780d419dad
Joseph Zheng <143029074+joz2005@users.noreply.github.com>
2025-02-13 05:17:50 +0000

    Test

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 """FastAPI main entrypoint file."""
 
-from fastapi import FastAPI
+from typing import Annotated, TypeAlias
+from typing import Annotated, TypeAlias
+from fastapi import FastAPI, Body, Depends
+from models import GamePlay, GameResult
+from services import GameService
 
 app = FastAPI()
 
-# TODO: Add routes here.
+GameServiceDI: TypeAlias = Annotated[GameService, Depends()]
+
+
+@app.post("/play")
+def play(
+    user_choice: Annotated[
+        GamePlay,
+        Body(description="User's choice of rock, paper, or scissors."),
+    ],
+    game_svc: GameServiceDI,
+) -> GameResult:
+    return game_svc.play(user_choice)
+
+
+@app.get("/results")
+def log(game_svc: GameServiceDI) -> list[GameResult]:
+    return game_svc.get_results()
